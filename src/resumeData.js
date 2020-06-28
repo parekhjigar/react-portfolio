b9d91057fdbf7298390b38e46aa9925ff152b805
parekhjigar <hello@parekhjigar.com>
2020-06-28 20:34:20 +1000

    File to store static data

---
+++ src/resumeData.js
@@ -0,0 +1,24 @@
+let resumeData = {
+    "imagebaseurl":"https://parekhjigar.com",
+    "name": "Jigar Parekh",
+    "role": "Frontend Developer",
+    "linkedinId":"LinkedIn Id",
+    "roleDescription": "I like explore various parts of frontend development.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jigar-parekh-9988b499/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/parekhjigar",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently pursuing Master of Information Technology from RMIT University.",
+    "address":"Melbourne, Australia",
+    "website":"https://parekhjigar.com"
+  }
+  
+  export default resumeData;
