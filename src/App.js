4080a68605d606e731412b97aee5dbc8b459ad22
Jigar Parekh <jigar@Jigars-MacBook-Pro.local>
2020-06-29 23:06:16 +1000

    Added component for contact

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Header from './components/Header';
 import About from './components/About';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
+import ContactUs from './components/ContactUs';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header resumeData={resumeData}/>
         <About resumeData={resumeData}/>
+        <Contact resumeData={resumeData}/>
         <Footer resumeData={resumeData}/>
       </div>
     );
